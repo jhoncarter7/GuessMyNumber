bb82e66acbac723662d3a8eff2a2fe73db599305
jhoncarter7 <90902071+jhoncarter7@users.noreply.github.com>
2022-07-22 14:42:45 +0530

    update: script.js
    refactoring script.js

--- script.js
+++ script.js
@@ -1,33 +1,77 @@
 
 
 
-let guessNumber = document.querySelector(".randomNumber").textContent = Math.round(Math.random()*20);
+let guessNumber = Math.round(Math.random() * 20);
 
 // document.querySelector(".innerscore").textContent
 
 // document.querySelector(".indicator").textContent;
 
-let score = 20;
+const displayMassage = function(message){
+    document.querySelector(".indicator").innerHTML = message;
+};
 
-document.querySelector(".submitt").addEventListener("click", function(){
+let score = 20;
+let highscore = 0;
+
+document.querySelector(".submitt").addEventListener("click", function (){
     const currentNumber = Number(document.querySelector(".number").value);
-   
-    if(!currentNumber){
-        document.querySelector(".indicator").textContent = "please put the number";
+
+    if (!currentNumber) {
+        displayMassage("please put a number");
     }
-    else if(currentNumber === guessNumber){
-        document.querySelector(".indicator").textContent = "yep you did it !" ;
+    else if (currentNumber === guessNumber) {
+        displayMassage( "yep you did it !");
         score++;
         document.querySelector(".innerscore span").textContent = score;
+        document.querySelector("body").style.backgroundColor = "green";
+        document.querySelector(".randomNumber").textContent = guessNumber;
+
+        // for adding highscore
+        if (score > highscore) {
+            highscore = score;
+            document.querySelector(".highscore span").textContent = highscore;
+        }
     }
-     else if (currentNumber > guessNumber){
-        document.querySelector(".indicator").innerHTML = "📈 you are to high";
-        score--;
+    else if (currentNumber !== guessNumber) {
+
+        if (score > 1) {
+
+        displayMassage(currentNumber > guessNumber ? "📈 you are to high" : "You are to Low"); 
+            score--;
+            document.querySelector(".innerscore span").textContent = score;
+            document.querySelector("body").style.backgroundColor = "red";
+        }
+        else {
+            displayMassage("You lost the game");
+        }
+    }}
+    )
+    
+
+    //     else if(currentNumber < guessNumber) {
+
+    //         if(score > 1){
+    //             document.querySelector(".indicator").innerHTML = "you are to low";
+    //             score--;
+    //             document.querySelector(".innerscore span").textContent = score;
+    //             document.querySelector("body").style.backgroundColor = "red";
+    //         }
+    //         else{
+    //             document.querySelector(".indicator").innerHTML = "You lost the game";
+    //                 }  
+    //     }
+    // })
+
+
+    //  Reset the game button
+
+    document.querySelector(".btn").addEventListener("click", function () {
+        score = 20;
         document.querySelector(".innerscore span").textContent = score;
-     }
-     else if(currentNumber < guessNumber){
-        document.querySelector(".indicator").innerHTML = "📉 you are to low";
-        score--;
-        document.querySelector(".innerscore span").textContent = score;
-     }
-})
+        guessNumber = Math.round(Math.random() * 20);
+        document.querySelector("body").style.backgroundColor = "white";
+        document.querySelector(".number").value = "";
+        displayMassage("Guess the number.....");
+        document.querySelector(".randomNumber").textContent = "?";
+    })
